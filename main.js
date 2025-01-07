6ee15fbe5d11f16205e5359645186cff674a2df3
ayushrawat98 <aayushrawat0@gmail.com>
2025-01-07 19:01:00 +0530

    updated angular and added helmet

--- main.js
+++ main.js
@@ -10,37 +10,47 @@ const syncer = require('./models/sync')
 const ipgeoblock = require("node-ipgeoblock");
 const getipaddress = require('./middlewares/getipaddress')
 const createadmin  = require('./helper/createAdmin')
+const helmet = require("helmet")
 
 const app = express()
 
-//dev
 app.use(ipgeoblock({
 	geolite2: "./public/GeoLite2-Country.mmdb",
 	allowedCountries: [ "IN" ]
 }));
-app.use(express.static(path.join(__dirname, "public/angular")));
 
-//local
 // app.use(cors())
 
+app.use(helmet({
+    crossOriginResourcePolicy: false,
+}))
 
+app.use(express.static(path.join(__dirname, "public/angular")));
+
+//parse json data
 app.use(express.json())
+
+//get ip address of user
 app.use(getipaddress)
 
+//DB
 syncer()
 // createadmin()
+
+//routes
 app.use("/video", videorouter)
 app.use("/authenticate", authenticaterouter)
 app.use("/user", userrouter)
 app.use("/comment", commentrouter)
 app.use("/admin", adminrouter)
 
+//handle any error
 app.use((err, req, res, next) => {
     res.status(500).send('Something broke!')
 })
 
 
-
+//create server
 app.listen(process.env.PORT, (ex) => {
     console.log(process.env.PORT)
 })
---
+++ public/angular/chunk-QR6ALSSZ.js
@@ -0,0 +1,1 @@
+function t(r){return r&&r.value.trim()==""?{required:!0}:null}export{t as a};
