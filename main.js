bf8e100762fedaaeb57672d16546deb11e8e395a
ayushrawat98 <aayushrawat0@gmail.com>
2025-01-07 20:06:07 +0530

    checking helmet

--- main.js
+++ main.js
@@ -23,12 +23,8 @@ app.use(ipgeoblock({
 
 // app.use(cors())
 
-//remove strict transport security with https
-// app.use(helmet({
-//     crossOriginResourcePolicy: false,
-//     strictTransportSecurity: false,
-//     crossOriginOpenerPolicy: false,
-// }))
+// remove strict transport security with https
+app.use(helmet())
 
 app.use(express.static(path.join(__dirname, "public/angular")));
 
