81c5452767199e0961a0628b7cb69a03e1d94b11
ayushrawat98 <aayushrawat0@gmail.com>
2025-01-14 19:32:57 +0530

    secure ws

--- websocket/main.js
+++ websocket/main.js
@@ -1,9 +1,19 @@
 const WebSocket = require('ws')
 const fs = require('fs')
+const https = require('https');
 
-const wss = new WebSocket.Server({ port: 8080 }, () => {
-    console.log('running at port 8080')
-});
+// const wss = new WebSocket.Server({ port: 8080 }, () => {
+//     console.log('running at port 8080')
+// });
+
+const options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/bharattube.xyz/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/bharattube.xyz/fullchain.pem')
+};
+const server = https.createServer(options)
+
+const wss = new WebSocket.Server({server})
+
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
     console.log('received audio')
@@ -11,5 +21,8 @@ wss.on('connection', function connection(ws) {
     console.log(message)
     ws.send(message)
   });
-
 });
+
+server.listen(8080, ()=> {
+  console.log('at 8080 ws')
+})
