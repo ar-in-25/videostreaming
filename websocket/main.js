efaa2649d744d618ad770a16045939195383eaff
ayushrawat98 <aayushrawat0@gmail.com>
2025-01-15 16:09:17 +0530

    added webscoket voice chat

--- websocket/main.js
+++ websocket/main.js
@@ -14,14 +14,70 @@ const server = https.createServer(options)
 
 const wss = new WebSocket.Server({server})
 
+let Users = []
+let Names = []
+
 wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received audio')
-    fs.writeFileSync('received.ogg',message)
-    console.log(message)
-    ws.send(message)
+
+  ws.on('message', (message, isBinary) => {
+    isBinary ? onBinary(message, ws) : onText(JSON.parse(message), ws)
   });
+
+  ws.on('close', () => {
+    if( Users.indexOf(ws) == -1){
+      
+    }else{
+      onText({event : 'disconnect', name : Names[Users.indexOf(ws)]}, ws)
+    }
+  })
+
 });
+
+function onBinary(message, user){
+  // sendToEveryoneExceptUser(message, user)
+  Users.forEach(ws => ws.send(message))
+}
+
+function onText(message, user){
+  switch(message.event){
+    case "connect":
+      addUser(message.name, user)
+      sendToEveryoneExceptUser(JSON.stringify({name : message.name, event : 'joined'}), user)
+      break;
+    case "disconnect":
+      sendLeaveMessage(JSON.stringify({name : message.name, event : 'left'}))
+      removeUser(message.name, user)
+      break;
+  }
+}
+
+
+function addUser(name, ws) {
+  Users.push(ws)
+  Names.push(name)
+}
+
+function removeUser(name, ws) {
+  Users.splice(Users.indexOf(ws),1)
+  Names.splice(Names.indexOf(name),1)
+}
+
+//send to all except the current user
+function sendToEveryoneExceptUser(message, user){
+  let sendList = Users.filter(x => x != user)
+  sendList.forEach(ws => ws.send(message))
+}
+
+//disconnected user is already removed from list
+function sendLeaveMessage(message){
+  Users.forEach(x => x.send(message))
+}
+
+
+
+
+
+
 
 server.listen(8080, ()=> {
   console.log('at 8080 ws')
