29066d4fee37356c7ed176d75da182018d1daf93
ayushrawat98 <aayushrawat0@gmail.com>
2025-01-16 13:31:37 +0530

    websocket prod

--- websocket/main.js
+++ websocket/main.js
@@ -2,17 +2,17 @@ const WebSocket = require('ws')
 const fs = require('fs')
 const https = require('https');
 
-const wss = new WebSocket.Server({ port: 8080 }, () => {
-    console.log('running at port 8080')
-});
+// const wss = new WebSocket.Server({ port: 8080 }, () => {
+//     console.log('running at port 8080')
+// });
 
-// const options = {
-//     key: fs.readFileSync('/etc/letsencrypt/live/bharattube.xyz/privkey.pem'),
-//     cert: fs.readFileSync('/etc/letsencrypt/live/bharattube.xyz/fullchain.pem')
-// };
-// const server = https.createServer(options)
+const options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/bharattube.xyz/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/bharattube.xyz/fullchain.pem')
+};
+const server = https.createServer(options)
 
-// const wss = new WebSocket.Server({server})
+const wss = new WebSocket.Server({server})
 
 
 wss.on('connection', function connection(ws) {
@@ -64,6 +64,6 @@ function onBinary(data, ws){
 }
 
 
-// server.listen(8080, ()=> {
-//   console.log('at 8080 ws')
-// })
+server.listen(8080, ()=> {
+  console.log('at 8080 ws')
+})
